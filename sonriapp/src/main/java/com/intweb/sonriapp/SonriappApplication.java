f140357c12b2ede3d81d6f130eaf41421fb19c54
Jesus Llonto <llonto25jesus@gmail.com>
2026-04-17 00:11:04 -0500

    primer commit sonriapp

---
+++ sonriapp/src/main/java/com/intweb/sonriapp/SonriappApplication.java
@@ -0,0 +1,13 @@
+package com.intweb.sonriapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonriappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonriappApplication.class, args);
+	}
+
+}
---
+++ sonriapp/src/test/java/com/intweb/sonriapp/SonriappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intweb.sonriapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonriappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
